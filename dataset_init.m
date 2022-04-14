b966de4cd51e1392a08b048ec0eccafae04fca2a
koku3322 <70087298+koku3322@users.noreply.github.com>
2022-04-13 21:11:12 -0600

    updated dataset_init.m with new camera - body transformation matrix

--- dataset_init.m
+++ dataset_init.m
@@ -5,11 +5,11 @@
 % To go with automated blender images
 
 % image set path
-datasetPath = "new_datasets/44bennu_sim";body = "bennu";
-imgPath = datasetPath + '\bennu_automated_images\';
+% datasetPath = "new_datasets/90bennu_sim";body = "bennu";
+% imgPath = datasetPath + '\bennu_automated_images\';
 
-% datasetPath = "new_datasets/90itokawa_sim";body = "itokawa";
-% imgPath = datasetPath + '\itokawa_automated_images\';
+datasetPath = "new_datasets/90itokawa_sim";body = "itokawa";
+imgPath = datasetPath + '\itokawa_automated_images\';
 
 %camera frame is stationary
 %but to body, camera is always rotating, as well as sun
@@ -32,14 +32,16 @@ start_angle = -5;
 disp('phase angle = ')
 disp(phase)
 
-%%%initializing the camera frame weirdness
-%-90 rotation about second axis
-rot1 = [cosd(-90) 0 sind(-90); 0 1 0; -sind(-90) 0 cosd(-90)];
-% +/- 180 rotation about third axis
-rot2 = [cosd(180), sind(180), 0; -sind(180), cosd(180), 0; 0, 0, 1];
-%account for y being in the top corner
-rot3 = [1 0 0; 0 -1 0; 0 0 1];
+%%initializing the camera frame weirdness
+% %-90 rotation about second axis
+% rot1 = [cosd(-90) 0 sind(-90); 0 1 0; -sind(-90) 0 cosd(-90)];
+% % +/- 180 rotation about third axis
+% rot2 = [cosd(180), sind(180), 0; -sind(180), cosd(180), 0; 0, 0, 1];
+% % account for y being in the top corner
+% rot3 = [1 0 0; 0 -1 0; 0 0 1];
 
+% blender camera nonsense
+rot0 = [0 0 -1;0 1 0;1 0 0];
 for x = 1:n
     %current angle of rotation
     angle = (start_angle * (pi/180)) + (x*1) * (rotate_by * (pi/180)); %degree to radian
@@ -47,7 +49,8 @@ for x = 1:n
     rotmZYX(:,:,x) = eul2rotm(eul); %the rotation matrix applied to the body
     
     %body-to-camera frame rotation
-    CB(:,:,x) = (rot3*rot2*rot1*(rotmZYX(:,:,x)));
+%     CB(:,:,x) = (rot3*rot2*rot1*(rotmZYX(:,:,x)));
+    CB(:,:,x) = rot0*rotmZYX(:,:,x);
     sun_pos(x,:) = [-S(3)+C(3),-S(2)+C(2),-S(1)+C(1)];%sun position in camera frame
     cam_pos(x,:) = [0,0,0];%camera_position in camera frame
     %change img_name path to point towards wherever you saved your images
